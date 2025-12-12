4431ac159bb682797a06cfec50d65a4b5f0ac224
sg36sagargupta26 <sg36.sagargupta26@gmail.com>
2025-12-12 15:35:06 +0530

    Day04 : day04b

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import days.day04.Day04;
 void main() {
     Day04 day04 = new Day04();
     Instant start = Instant.now();
-    IO.println("result -> " + day04.day04a());
+    IO.println("result -> " + day04.day04b());
     Instant end = Instant.now();
     Duration duration = Duration.between(start,end);
     IO.println("time -> " +duration.toMillis());
--- src/days/day04/Day04.java
+++ src/days/day04/Day04.java
@@ -5,8 +5,10 @@ import utility.Direction;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
-
+record Pair(int i, int j){}
 public class Day04 {
     public char[][] getInput(){
         try(Stream<String> list = Files.lines(Paths.get("src/days/day04/day04a.txt"))){
@@ -18,6 +20,39 @@ public class Day04 {
             return null;
         }
     }
+    public int day04b() {
+        char [][] input = getInput();
+        int result =0;
+        int currResult =1;
+        int rowLen = input.length;
+        int colLen = input[0].length;
+        while (currResult>0){
+            currResult =0;
+            List<Pair> pairs = new ArrayList<>();
+            for(int i =0;i<rowLen;i++){
+                for(int j=0;j<colLen;j++){
+                    if(input[i][j]!='@'){
+                        continue;
+                    }
+                    int count =0;
+                    for(Direction direction : Direction.values()){
+                        if(validDirection(direction,i,j,input)){
+                            count++;
+                        }
+                    }
+                    if(count<4){
+                        pairs.add(new Pair(i,j));
+                        currResult++;
+                    }
+                }
+            }
+            pairs.forEach(a->input[a.i()][a.j()]='.');
+            result+=currResult;
+        }
+
+        return result;
+    }
+
     public int day04a() {
         char [][] input = getInput();
         int result =0;
@@ -50,5 +85,6 @@ public class Day04 {
         }
         return input[i][j] == '@';
     }
+
 
 }
