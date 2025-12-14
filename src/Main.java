84dd0ea47a312e4446446577fe9d19f8e4731ebf
sg36sagargupta26 <sg36.sagargupta26@gmail.com>
2025-12-14 20:37:23 +0530

    Day06.java : doy06 txt changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import days.day06.Day06;
 void main() {
     Day06 day06 = new Day06();
     Instant start = Instant.now();
-    IO.println("result -> " + day06.day06a());
+    IO.println("result -> " + day06.day06b());
     Instant end = Instant.now();
     Duration duration = Duration.between(start,end);
     IO.println("time -> " +duration.toMillis());
--- src/days/day06/Day06.java
+++ src/days/day06/Day06.java
@@ -5,6 +5,8 @@ import utility.Operand;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Day06 {
@@ -23,6 +25,40 @@ public class Day06 {
         return res;
     }
 
+    public long day06b() {
+        var input = getSecondInput();
+        long res = 0;
+        List<Long> currRes = new ArrayList<>();
+        Operand operand = Operand.getOperand(String.valueOf(input[input.length-1][0]));
+        for(int i =0;i<input[0].length;i++){
+            long x = 0;
+            boolean anyElementAdded = false;
+            for(int j = 0;j< input.length-1;j++){
+                if(input[j][i]==' '){
+                    continue;
+                }
+                anyElementAdded = true;
+                x *= 10;
+                x += input[j][i]-'0';
+            }
+            if(anyElementAdded){
+                currRes.add(x);
+            }
+
+            if((i==input[0].length-1 || input[input.length-1][i+1]!=' ') ){
+                long curr = operand.starter();
+                for(long l: currRes){
+                    curr=operand.doOperation(l,curr);
+                }
+                res+=curr;
+                currRes=new ArrayList<>();
+                operand = Operand.getOperand(String.valueOf(input[input.length-1][i+1]));
+
+            }
+        }
+        return res;
+    }
+
     private String [][] getInput(){
         try(Stream<String> lines = Files.lines(Path.of("src/days/day06/day06.txt"))){
             var list = lines
@@ -32,6 +68,17 @@ public class Day06 {
             return list.toArray(new String[list.size()][]);
         } catch (IOException e) {
             return new String[0][0];
+        }
+    }
+
+    private char [][] getSecondInput(){
+        try(Stream<String> lines = Files.lines(Path.of("src/days/day06/day06.txt"))){
+            var list = lines
+                    .map(String::toCharArray)
+                    .toList();
+            return list.toArray(new char[list.size()][]);
+        } catch (IOException e) {
+            return new char[0][0];
         }
     }
 
