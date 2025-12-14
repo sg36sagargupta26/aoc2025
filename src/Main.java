06098ea81e402afbedf7a8b3dd95c4fe39ac5195
sg36sagargupta26 <sg36.sagargupta26@gmail.com>
2025-12-14 22:54:47 +0530

    Day07.java : day07 b

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import days.day07.Day07;
 void main() {
     Day07 day07 = new Day07();
     Instant start = Instant.now();
-    IO.println("result -> " + day07.day07a());
+    IO.println("result -> " + day07.day07b());
     Instant end = Instant.now();
     Duration duration = Duration.between(start,end);
     IO.println("time -> " +duration.toMillis());
--- src/days/day07/Day07.java
+++ src/days/day07/Day07.java
@@ -3,7 +3,9 @@ package days.day07;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -37,6 +39,44 @@ public class Day07 {
             set = nextSet;
         }
         return count;
+    }
+
+    public long day07b() {
+        var input = input();
+        int startPosition  = 0;
+        while (startPosition<input[0].length && input[0][startPosition]!='S'){
+            startPosition++;
+        }
+        Map<Integer,Long> map = new HashMap<>();
+        map.put(startPosition,1L);
+        int currentLevel = 1;
+        while (!map.isEmpty() && currentLevel<input.length){
+            var nextMap = getNextMap(map, input, currentLevel);
+            currentLevel++;
+            map = nextMap;
+        }
+        return map
+                .values()
+                .stream()
+                .mapToLong(Long::longValue)
+                .sum();
+    }
+
+    private Map<Integer, Long> getNextMap(Map<Integer, Long> map, char[][] input, int currentLevel) {
+        Map<Integer,Long> nextMap = new HashMap<>();
+        for(var c : map.entrySet()){
+            if(input[currentLevel][c.getKey()]=='.'){
+                nextMap.put(c.getKey(),nextMap.getOrDefault(c.getKey(),0L)+c.getValue());
+            }else if(input[currentLevel][c.getKey()]=='^'){
+                if(c.getKey()-1>=0){
+                    nextMap.put(c.getKey()-1,nextMap.getOrDefault(c.getKey()-1,0L)+c.getValue());
+                }
+                if(c.getKey()+1< input[0].length){
+                    nextMap.put(c.getKey()+1,nextMap.getOrDefault(c.getKey()+1,0L)+c.getValue());
+                }
+            }
+        }
+        return nextMap;
     }
 
     private char[][] input(){
